3f44a7658e66c628934fe91f23a09de4064a6aae
raddevus <1radeutsch1@gmail.com>
2023-07-16 11:53:27 -0400

    Initial Commit

---
+++ IExpense/IExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  IExpenseApp.swift
+//  IExpense
+//
+//  Created by roger deutsch on 7/16/23.
+//
+
+import SwiftUI
+
+@main
+struct IExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
